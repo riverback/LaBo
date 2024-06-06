23f5ea9c6f941bb37c19ff9b28e4d883d431229e
riverback <hhz_pku@163.com>
2024-06-06 23:01:33 +0800

    save bash scripts

--- labo_test.sh
+++ labo_test.sh
@@ -1,3 +1,4 @@
 # $1: cfg path
 # $2: ckpt path
+# bash labo_test.sh config_path ckpt_path
 python main.py --cfg $1 --func asso_opt_main --test --cfg-options bs=512 ckpt_path=$2 ${@:3}
--- labo_train.sh
+++ labo_train.sh
@@ -1,4 +1,5 @@
 # $1: number of shots
 # $2: dataset (flower/food101)
+# bash labo_train.sh 1 flower
 
 python main.py --cfg cfg/asso_opt/$2/$2_$1shot_fac.py --work-dir exp/asso_opt/$2/$2_$1shot_fac --func asso_opt_main ${@:3}
