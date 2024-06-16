13c7d1d3279e2920f05ebc46cbf77efac14a0b83
riverback <hhz_pku@163.com>
2024-06-16 13:53:08 +0000

    test new git

--- temp.sh
+++ temp.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food 1 ViT-L/14
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food 2 ViT-L/14
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food 4 ViT-L/14
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food 8 ViT-L/14
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food 16 ViT-L/14
-CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh food all ViT-L/14
+#CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 1 ViT-L/14
+CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 2 ViT-L/14
+CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 4 ViT-L/14
+CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 8 ViT-L/14
+CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 16 ViT-L/14
+CUDA_VISIBLE_DEVICES=0 sh linear_probe.sh HAM10000 all ViT-L/14
 
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 1 food
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 2 food
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 4 food
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 8 food
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 16 food
-CUDA_VISIBLE_DEVICES=0 bash labo_train.sh all food
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 1 HAM10000
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 2 HAM10000
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 4 HAM10000
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 8 HAM10000
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh 16 HAM10000
+# CUDA_VISIBLE_DEVICES=0 bash labo_train.sh all HAM10000
